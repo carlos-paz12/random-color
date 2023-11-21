ac36ac1ba410c956a368da46aa96e0ef018c1c4e
Carlos Paz <147278027+carlos-paz12@users.noreply.github.com>
2023-11-21 14:41:19 -0300

    Separate files

---
+++ script.js
@@ -0,0 +1,15 @@
+const button = document.querySelector('button');
+const body = document.querySelector('body');
+let p = document.querySelector('p');
+
+button.onclick = function () {
+    let randomColor = '#';
+
+    while (randomColor.length < 7) {
+        randomColor += Math.floor(Math.random() * 0x10).toString(16);
+    }
+
+    body.style.backgroundColor = randomColor;
+
+    p.innerHTML = randomColor;
+};
